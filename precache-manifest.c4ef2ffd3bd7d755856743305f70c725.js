b4880fbe0ba94f6875e85e10ede1264017a2c776
SachinRajkumarSaini <sachinrajkumarsaini2002@gmail.com>
2020-09-09 20:14:31 +0530

    Updates

---
+++ precache-manifest.c4ef2ffd3bd7d755856743305f70c725.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f6a4c7cbd183a9ce0990443926eda5f",
+    "url": "/CodeWithCoder/index.html"
+  },
+  {
+    "revision": "36c25ed7e6a6e91001b3",
+    "url": "/CodeWithCoder/static/css/2.78e0feef.chunk.css"
+  },
+  {
+    "revision": "4cb69c981a07d151ceea",
+    "url": "/CodeWithCoder/static/css/main.93d6a104.chunk.css"
+  },
+  {
+    "revision": "36c25ed7e6a6e91001b3",
+    "url": "/CodeWithCoder/static/js/2.8afd3081.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/CodeWithCoder/static/js/2.8afd3081.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cb69c981a07d151ceea",
+    "url": "/CodeWithCoder/static/js/main.b9a48a9c.chunk.js"
+  },
+  {
+    "revision": "d1f7144fd950e6318683",
+    "url": "/CodeWithCoder/static/js/runtime-main.95f198b2.js"
+  },
+  {
+    "revision": "d0d141c40e9c40289b57a90dcf9b88ff",
+    "url": "/CodeWithCoder/static/media/Aboutimg.d0d141c4.png"
+  },
+  {
+    "revision": "52bec8f6a5fb989a25429cd17b52c036",
+    "url": "/CodeWithCoder/static/media/about-us.52bec8f6.png"
+  },
+  {
+    "revision": "50953027678f2e493ea34b63733f26cd",
+    "url": "/CodeWithCoder/static/media/banner-image.50953027.png"
+  },
+  {
+    "revision": "c93e770da206ad007853d6f3917278e2",
+    "url": "/CodeWithCoder/static/media/contact-1.c93e770d.png"
+  }
+]);
